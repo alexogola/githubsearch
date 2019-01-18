1f4478d03989fbe88e9a5fae59e03f2fce7bba33
Alex Ogola <ogollaokumu@gmail.com>
2019-01-18 12:24:26 +0300

    fix services error

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-Import 'rxjs/add/operator/map';
+import {Injectable} from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,21 @@ export class GithubService {
   private client_id = 'f9c23f736d6ecbbe42ec';
   private client_secret = 'ff510d303934c61f69d04c849d943ba15d7a4940';
 
-  constructor(private _http:Http) {
-   console.log('Github service init...');
-   }
-}
+  constructor(private _http:Http){
+          console.log('Github Service Init...');
+      }
+
+      getUser(){
+          return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+              .map(res => res.json());
+      }
+
+      getRepos(){
+          return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+              .map(res => res.json());
+      }
+
+      updateUsername(username:string){
+          this.username = username;
+      }
+  }
