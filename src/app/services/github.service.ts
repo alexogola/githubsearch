98f5515d953eb9e518601a9ae6a136bde6947f45
Alex Ogola <ogollaokumu@gmail.com>
2019-01-18 11:56:33 +0300

    register github service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+Import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  private username = 'lex-of-pakawa';
+  private client_id = 'f9c23f736d6ecbbe42ec';
+  private client_secret = 'ff510d303934c61f69d04c849d943ba15d7a4940';
+
+  constructor(private _http:Http) {
+   console.log('Github service init...');
+   }
 }
