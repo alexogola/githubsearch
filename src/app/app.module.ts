291819295b68fc0c10830b27ca11fa01df47a2dc
Alex Ogola <ogollaokumu@gmail.com>
2019-01-19 14:15:35 +0300

    install rxjs compact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { FormsModule } from '@angular/forms';
+
 import { ProfileService } from './services/profile.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GithubComponent } from './components/github/github.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent,
-    ProfileComponent,
-    HttpModule
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    FormsModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProfileService {
 
   private username:string;
@@ -12,13 +10,22 @@ export class ProfileService {
   private clientsecret = 'ff510d303934c61f69d04c849d943ba15d7a4940';
 
   constructor(private http:Http) {
-    console.log("service is now ready!");
-    this.username = 'lex-of-pakawa';
-   }
+  	console.log("service is now ready!");
+  	this.username = 'lex-of-pakawa';
+  }
 
-   getProfileInfo(){
-   	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-   	.map(res => res.json());
-   }
+  getProfileInfo(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  getProfileRepos(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  updateProfile(username:string){
+  	this.username = username;
+  }
 
 }
