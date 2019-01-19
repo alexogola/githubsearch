9f22ebeee03f827b7cea432781c0b5071861018c
Alex Ogola <ogollaokumu@gmail.com>
2019-01-19 12:47:26 +0300

    create properties for the profile service class

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,5 +7,9 @@ import 'rxjs/add/operator/map';
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private clientid = 'f9c23f736d6ecbbe42ec';
+  private clientsecret = 'ff510d303934c61f69d04c849d943ba15d7a4940';
+
+  constructor(private http:Http) { }
 }
