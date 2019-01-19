bb2fd76e1e52d0c03a90bf14b4ea805dad7ccd54
Alex Ogola <ogollaokumu@gmail.com>
2019-01-19 13:01:00 +0300

    import profile service into appmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ProfileService } from './services/profile.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './components/github/github.component';
@@ -16,7 +18,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
