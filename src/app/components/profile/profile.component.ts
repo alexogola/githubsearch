8572b3d6ac0adcbff90f346b1194d12fa125f2e1
Alex Ogola <ogollaokumu@gmail.com>
2019-01-19 14:42:39 +0300

    install bootstrap and jquery

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,11 +7,25 @@ import { ProfileService } from '../../services/profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile:any[];
+  repos: any[];
+  username:string;
 
   constructor(private profileService: ProfileService) {
-    	this.profileService.getProfileInfo().subscribe(profile => {
-    		console.log(profile);
-    	});
+
+  }
+
+  findProfile(){
+  	this.profileService.updateProfile(this.username);
+  	this.profileService.getProfileInfo().subscribe(profile => {
+  		console.log(profile);
+  		this.profile = profile;
+  	});
+
+  	this.profileService.getProfileRepos().subscribe(repos => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})
   }
 
   ngOnInit() {
